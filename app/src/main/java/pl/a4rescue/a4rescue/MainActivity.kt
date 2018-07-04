8fd579281d94714bcb549d3ede0a1bd5009bd80a
Bartosz Dabek <elderaramka@gmail.com>
2018-07-04 21:44:39 +0200

    add support for use JUnit5

--- app/src/main/java/pl/a4rescue/a4rescue/MainActivity.kt
+++ app/src/main/java/pl/a4rescue/a4rescue/MainActivity.kt
@@ -1,12 +1,18 @@
 package pl.a4rescue.a4rescue
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startBtn.setOnClickListener {
+            Log.d("TAG", "Start clicked")
+        }
     }
 }
---
+++ app/src/test/java/pl/a4rescue/a4rescue/MainActivityTest.kt
@@ -0,0 +1,15 @@
+package pl.a4rescue.a4rescue
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+
+
+
+class MainActivityTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
