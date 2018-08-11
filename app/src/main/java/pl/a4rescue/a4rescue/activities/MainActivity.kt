7846d30fd2f0c1f8dd27b65b5fb2e28287bbc2d6
Bartosz Dąbek <elderaramka@gmail.com>
2018-08-11 15:16:57 +0200

    refactor

--- app/src/main/java/pl/a4rescue/a4rescue/activities/MainActivity.kt
+++ app/src/main/java/pl/a4rescue/a4rescue/activities/MainActivity.kt
@@ -1,11 +1,10 @@
 package pl.a4rescue.a4rescue.activities
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
-import android.support.v7.app.ActionBar
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import pl.a4rescue.a4rescue.R
@@ -20,35 +19,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(toolbar)
-        val actionbar: ActionBar? = supportActionBar
-        actionbar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-            setHomeAsUpIndicator(R.drawable.ic_menu)
-        }
+        setupDrawerToggle()
+        setupDrawerContent()
 
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, HomeScreenFragment())
+                    .commit()
+            nav_view.setCheckedItem(R.id.nav_home)
+        }
+    }
+
+    private fun setupDrawerToggle() {
         val actionBarDrawerToggle = ActionBarDrawerToggle(this, drawer_layout, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
+    }
 
+    private fun setupDrawerContent() {
         nav_view.setNavigationItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.nav_home -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,
-                        HomeScreenFragment()).commit()
-                R.id.nav_contacts -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,
-                        ContactFragment()).commit()
-            }
-            drawer_layout.closeDrawers()
-
+            selectDrawerItem(menuItem)
             true
         }
+    }
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction().replace(R.id.fragment_container,
-                    HomeScreenFragment()).commit()
-            nav_view.setCheckedItem(R.id.nav_home)
+    private fun selectDrawerItem(menuItem: MenuItem) {
+        lateinit var fragment: Fragment
+
+        when (menuItem.itemId) {
+            R.id.nav_home -> fragment = HomeScreenFragment()
+            R.id.nav_contacts -> fragment = ContactFragment()
         }
 
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit()
+
+        drawer_layout.closeDrawers()
     }
 
     override fun onBackPressed() {
@@ -57,36 +65,6 @@ class MainActivity : AppCompatActivity() {
         } else {
             super.onBackPressed()
         }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                drawer_layout.openDrawer(GravityCompat.START)
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d("TAG", "ON RESUME MainActivity")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d("TAG", "ON PAUSE MainActivity")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d("TAG", "ON STOP MainActivity")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("TAG", "ON DESTROY MainActivity")
     }
 
 }
