95441eeae848057ef00c2309f0b9e2a86caf0ed7
Bartosz Dąbek <elderaramka@gmail.com>
2018-08-29 21:36:33 +0200

    add recycler view for contacts and possibility to delete contact by swipe

---
+++ app/src/main/java/pl/a4rescue/a4rescue/contact/SwipeHelper.kt
@@ -0,0 +1,21 @@
+package pl.a4rescue.a4rescue.contact
+
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.helper.ItemTouchHelper
+import android.util.Log
+import pl.a4rescue.a4rescue.adapter.ContactListAdapter
+
+class SwipeHelper(private val adapter: ContactListAdapter) : ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP, ItemTouchHelper.LEFT) {
+
+    private val TAG = SwipeHelper::class.java.simpleName
+
+    override fun onMove(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?, target: RecyclerView.ViewHolder?): Boolean {
+        return false
+    }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        Log.d(TAG, "Item on position: ${viewHolder.adapterPosition} is swiped")
+        adapter.removeContact(viewHolder.adapterPosition)
+    }
+
+}
