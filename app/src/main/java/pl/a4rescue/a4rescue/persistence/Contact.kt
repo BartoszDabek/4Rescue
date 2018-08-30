83e5749130ab9df59e0757a95e48cd9a6074030f
Bartosz Dąbek <elderaramka@gmail.com>
2018-08-30 13:58:10 +0200

    change contact primary key to phone number and deny to add duplicate contacts

--- app/src/main/java/pl/a4rescue/a4rescue/persistence/Contact.kt
+++ app/src/main/java/pl/a4rescue/a4rescue/persistence/Contact.kt
@@ -7,14 +7,10 @@ import android.arch.persistence.room.PrimaryKey
 
 @Entity(tableName = "contacts")
 data class Contact (
-        @PrimaryKey(autoGenerate = true)
-        val id: Long,
-
         @ColumnInfo(name = "user_name")
         val userName: String,
 
+        @PrimaryKey
         @ColumnInfo(name = "phone_number")
         val phoneNumber: String
-    ) {
-    constructor(name: String, phoneNumber: String): this(0L, name, phoneNumber)
-}
+)
